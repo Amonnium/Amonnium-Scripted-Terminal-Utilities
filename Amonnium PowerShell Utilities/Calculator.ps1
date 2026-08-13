0ca298f29320de89908a980af3bd8debc3d56eee
João Diogo <158088433+Amonnium@users.noreply.github.com>
2026-08-13 18:21:06 +0100

    Add files via upload

---
+++ Amonnium PowerShell Utilities/Calculator.ps1
@@ -0,0 +1,48 @@
+﻿## Create Calculator using arithmetic operators in PowerShell
+
+Write-Host "Welcome to our Calculator utility"
+
+$repeat = "Yes"
+
+while($repeat -eq "Yes")
+{
+[int]$firstNumber = Read-Host "Enter the first number"
+[int]$secondNumber = Read-Host "Enter the second number"
+
+$operation = Read-Host "What operation you want to perform (+, -, *, /, %)"
+
+Switch($operation)
+{
+    +
+    {
+        Write-Host("Result of your selected operation($operation) is:")
+        $firstNumber + $secondNumber
+    }
+    -
+    {
+        Write-Host("Result of your selected operation($operation) is:")
+        $firstNumber - $secondNumber
+    }
+    *
+    {
+        Write-Host("Result of your selected operation($operation) is:")
+        $firstNumber * $secondNumber
+    }
+    /
+    {
+        Write-Host("Result of your selected operation($operation) is:")
+        $firstNumber / $secondNumber
+    }
+    %
+    {
+        Write-Host("Result of your selected operation($operation) is:")
+        $firstNumber % $secondNumber
+    }
+    default
+    {
+        Write-Host("Please select one of the available operations")
+    }
+}
+
+$repeat = Read-Host "Do you want to perform another action(Yes/No)?"
+}
---
+++ Amonnium PowerShell Utilities/CleanTemp.ps1
@@ -0,0 +1,11 @@
+﻿$tempFolders = @(
+    $env:TEMP
+    "C:\Windows\Temp"
+)
+
+foreach ($folder in $tempFolders) {
+    if (Test-Path $folder) {
+        Get-ChildItem -Path $folder -File -Force -ErrorAction SilentlyContinue |
+            Remove-Item -Force -ErrorAction SilentlyContinue
+    }
+}
